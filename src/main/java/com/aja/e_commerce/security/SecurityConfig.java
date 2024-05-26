2d46372d26ad2a18614d3068dd7462016f8a3ffa
Ahmet Özcan <ahmetozcan21@yahoo.com>
2024-05-26 15:55:54 +0300

    removed unnecesseary codes and imports

--- src/main/java/com/aja/e_commerce/security/SecurityConfig.java
+++ src/main/java/com/aja/e_commerce/security/SecurityConfig.java
@@ -5,16 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -22,18 +16,16 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector)
-            throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        MvcRequestMatcher.Builder mvcRequestBuilder = new MvcRequestMatcher.Builder(introspector);
 
         http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((request) -> request
-                        .requestMatchers(mvcRequestBuilder.pattern("/")).permitAll()
-                        .requestMatchers(mvcRequestBuilder.pattern("/api/public/**")).permitAll()
-                        .requestMatchers(mvcRequestBuilder.pattern("/api/private/**")).authenticated()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/api/private/**").authenticated()
                         .anyRequest().permitAll())
                 .formLogin(form -> form.loginPage("/login")
                         .loginProcessingUrl("/login/process")
